bf6f83dcbdfa762821ec2057cbac2f9802717a4b
Unbuttun <unbuttun@gmail.com>
2021-07-31 23:44:07 +0530

    fix(build): 🐛 Fixing the build and various stuff through esbuild and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { FastifyInstance } from 'fastify';
-import fastifyPlugin from 'fastify-plugin';
-import { createPool, sql, TaggedTemplateLiteralInvocationType } from 'slonik';
-import { createQueryLoggingInterceptor } from 'slonik-interceptor-query-logging';
+import { FastifyInstance } from "fastify";
+import fastifyPlugin from "fastify-plugin";
+import { createPool, sql, TaggedTemplateLiteralInvocationType } from "slonik";
+import { createQueryLoggingInterceptor } from "slonik-interceptor-query-logging";
 
 type SlonikOptions = {
   connectionString: string;
@@ -12,6 +12,7 @@ const fastifySlonik = async (
   fastify: FastifyInstance,
   options: SlonikOptions
 ) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const interceptors = [createQueryLoggingInterceptor()];
   const { connectionString, queryLogging } = options;
   const pool = createPool(connectionString, {
@@ -19,14 +20,16 @@ const fastifySlonik = async (
   });
 
   try {
+    // eslint-disable-next-line @typescript-eslint/require-await
     await pool.connect(async () => {
-      fastify.log.info('Connected to Postgres DB');
+      fastify.log.info("Connected to Postgres DB");
     });
   } catch (err) {
-    fastify.log.error('Error happened while connecting to Postgres DB', err);
+    fastify.log.error("Error happened while connecting to Postgres DB", err);
   }
 
   async function transaction(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     connection: TaggedTemplateLiteralInvocationType<unknown>
   ) {
@@ -35,17 +38,17 @@ const fastifySlonik = async (
 
   const db = {
     connect: pool.connect.bind(pool),
-    pool: pool,
+    pool,
     query: pool.query.bind(pool),
     transaction: transaction.bind(pool),
     exists: pool.exists.bind(pool),
   };
 
-  fastify.decorate('slonik', db);
-  fastify.decorate('sql', sql);
+  fastify.decorate("slonik", db);
+  fastify.decorate("sql", sql);
 };
 
 export default fastifyPlugin(fastifySlonik, {
-  fastify: '3.x',
-  name: 'fastify-slonik',
+  fastify: "3.x",
+  name: "fastify-slonik",
 });
