9ab7d58574724889271ece218edfbd25b09bb93f
Spark <79936503+spa5k@users.noreply.github.com>
2022-07-13 23:11:44 +0530

    feat(build): 💚 removing minification from build, removing source map
    no longer minification or sourcemap

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,11 @@ import type { Options } from "tsup";
 const env = process.env.NODE_ENV;
 
 export const tsup: Options = {
-  splitting: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   dts: true,
   format: ["cjs", "esm"],
-  minify: true,
+  minify: false,
   bundle: true,
   watch: env === "development",
   skipNodeModulesBundle: true,
