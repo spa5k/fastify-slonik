59de3f2d69d951fbaa5519ec109df4d556fb1e09
Olivier Pichon <203745+opichon@users.noreply.github.com>
2023-07-24 13:42:08 +0700

    Client configuration (#99)
    * fixed use of createDatabasePool as async function
    
    * update dependencies
    
    * fix(upgrade): add async await
    
    * revert: package.json
    
    * test: ignore test file when release
    
    * test: update test for latest slonik
    
    * test: fix lint issue
    
    * feat:add support for slonik's client configuration when creating the connection pool
    
    * fix: change clientConfiguration type to ClientConfigurationInput
    
    ---------
    
    Co-authored-by: Dipendra Upreti <dipendra@dzangolab.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import fastifyPlugin from "fastify-plugin";
 import type { DatabasePool } from "slonik";
 import { createPool, sql } from "slonik";
 import type {
+  ClientConfigurationInput,
   ConnectionRoutine,
   QueryFunction,
   SqlTaggedTemplate,
@@ -11,6 +12,7 @@ import type {
 
 type SlonikOptions = {
   connectionString: string;
+  clientConfiguration?: ClientConfigurationInput
 };
 
 // using declaration merging, add your plugin props to the appropriate fastify interfaces
@@ -35,10 +37,10 @@ declare module "fastify" {
 }
 
 const plugin = async (fastify: FastifyInstance, options: SlonikOptions) => {
-  const { connectionString } = options;
+  const { connectionString, clientConfiguration } = options;
   let pool: DatabasePool;
   try {
-    pool = await createPool(connectionString);
+    pool = await createPool(connectionString, clientConfiguration);
   } catch (error) {
     fastify.log.error("🔴 Error happened while connecting to Postgres DB");
     throw new Error(error as string);
